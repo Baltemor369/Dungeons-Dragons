b32ade9ca8b62e3b85b7c5d5388d6e758a0d3def
Baltemor369 <118623957+Baltemor369@users.noreply.github.com>
2023-12-05 00:10:10 +0100

    start pre config weapon and monsters

--- Dungeon&Dragons_v1/Const.h
+++ Dungeon&Dragons_v1/Const.h
@@ -5,6 +5,11 @@
 #include <map>
 #include <string>
 
+#include "Weapon.h"
+#include "Armor.h"
+#include "Ressources.h"
+
+
 namespace Const {
     const std::vector<std::string> DIRECTIONS = {"up", "down","right","left"};
     const int MAP_HEIGHT_MAX = 10;
@@ -12,10 +17,39 @@ namespace Const {
     const int MAP_HEIGHT_MIN = -10;
     const int MAP_WIDTH_MIN = -10;
 
-    //monsters specifications :
-    std::vector<std::string> MOB_NAMES = {
-        "Dragon", "Goblin", "Ogre","skeleton","zombie"
-    }
+    struct WeaponsParams {
+        std::string name;
+        int damage;
+        int letha;
+        int reach;
+    };
+    std::map<std::string, WeaponsParams> weaponsParamsDict;
+
+    WeaponsParams ;
+    
+    struct MonsterParams {
+        std::string name;
+        int hp;
+        int velocity;
+        Weapon weapon;
+        Armor armor;
+        Ressources ressources;
+    };
+    std::map<std::string, MonsterParams> monsterParamsDict;
+
+    MonsterParams goblinParams = {"Goblin", 10, 3, sword};
+    MonsterParams orcParams = {};
+    MonsterParams dragonParams = {};
+    MonsterParams shadowspireParams = {};
+    MonsterParams blihtbeastParams = {};
+    MonsterParams thundermawParams = {};
+    MonsterParams frostbaneParams = {};
+    MonsterParams venomshadeParams = {};
+    MonsterParams emberfiendParams = {};
+    MonsterParams dreadhowlParams = {};
+    MonsterParams runebladeParams = {};
+    MonsterParams lunarshroudParams = {};
+    MonsterParams neanterosParams = {};
     
 }
 
